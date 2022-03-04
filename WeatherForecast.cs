cc137e268427bbde9de0c75d7f849164b1974eac
wayn3r <w9delacruz@gmail.com>
2022-03-04 17:10:05 -0400

    add .net with react template

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace wayner_de_la_cruz;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
